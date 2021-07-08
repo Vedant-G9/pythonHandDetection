6076f86bbc0688024c018273b11b4582a3989fd4
Vedant-G9 <vedantgosai007@gmail.com>
2021-07-09 00:46:00 +0530

    pythonFaceDetection

---
+++ project_exp.py
@@ -0,0 +1,20 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+pTime = 0
+ctime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img, draw=True)
+    lmlist = detector.findPosition(img, draw=False)
+
+    if len(lmlist) !=0:
+        print(lmlist[4])
+
+    cv2.imshow('output', img)
+    cv2.waitKey(1)
